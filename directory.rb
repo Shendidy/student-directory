e8adcc4caf715f2de567abe43dbb6e95a38fb64e
Shendidy <sherif@shendidy.com>
2018-12-24 05:54:09 +0000

    Added .gitignore file

--- directory.rb
+++ directory.rb
@@ -65,6 +65,25 @@ def input_students
     entry = gets.chomp.split("*")
   end
 end
+
+
+
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of studentds
+  @students.each do |student|
+    student_data = [student[:name], student[:hobby], student[:country], student[:height], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+
+
+
 def print_header
   puts
   puts "The students of Villains Academy".center(120)
@@ -95,6 +114,7 @@ end
 def print_menu
   puts "1. Input the students list"
   puts "2. Show the students list"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we'll be adding more items/options/choices
 end
 def process(selection)
@@ -103,6 +123,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit # this will cause the program to terminate
   else
